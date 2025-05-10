7522f9c15b5ca6e4318c23b92d9175979255fb09
thenujawickramarachchi <thisarithenuja@gmail.com>
2025-05-10 13:48:56 +0530

    nutrition 1

--- frontend/src/MealPlaning/NutritionPage.jsx
+++ frontend/src/MealPlaning/NutritionPage.jsx
@@ -227,6 +227,7 @@ export default function NutritionPage() {
             </motion.div>
 
             {/* Dairy Items */}
+            
             <motion.div
               variants={cardVariants}
               initial="hidden"
